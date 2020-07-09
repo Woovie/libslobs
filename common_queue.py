3b73b8298db5590c9313f1f0f1ce8e5e0e05ea2f
Jordan Banasik <woovie@woovie.net>
2020-07-09 01:12:19 -0700

    First commit of real queue code

--- common_queue.py
+++ common_queue.py
@@ -1,6 +1,12 @@
+from typing import Callable
 
 class SLOBSQueue():
     def __init__(self):
         self.incoming = []
-        self.outgoing = []
-        self.events   = []
+        self.events = {}
+
+    def add_callback(self, callback: Callable[[str], str], id: str):
+        self.events[id] = callback
+
+    def remove_callback(self, id: str):
+        self.events.pop(id)
