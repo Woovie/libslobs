3bc03498e7be089c9854b8bf93e1ff722df2a697
Jordan Banasik <woovie@woovie.net>
2020-07-09 01:10:30 -0700

    Added type hinting, removed config code
    Modifying SLOBSClient to be more of a true library and less of a custom
    piece of code just for me.
    * Added type hinting
    * Require URL and auth to be passed to SLOBSClient

--- common_client.py
+++ common_client.py
@@ -1,24 +1,23 @@
 import uuid
 import libslobs.common_queue as queue
 import libslobs.common_websocket as websocket
-import libslobs.common_config as config
 import libslobs.common_payloads as payloads
 
 class SLOBSClient():
-    def __init__(self):
-        self.config = config.SLOBSConfig().config
+    def __init__(self, auth_token: str, url: str):
+        self.url = url
+        self.auth_token = auth_token
         self.connection_string = str(uuid.uuid1())
-        self.slobs_websocket_url = f"{self.config['slobs-client']['url']}{self.connection_string}/websocket"
+        self.slobs_websocket_url = f"{self.url}{self.connection_string}/websocket"
         self.connection_handler = websocket.SLOBSWebSocket(self.slobs_websocket_url)
-        self.auth_token = self.config['slobs-client']['api-token']
         self.queue = queue.SLOBSQueue()
 
     def connect(self):
         return self.connection_handler.connect()
 
     def auth(self):
-        payload = payloads.SLOBSPayloads().create_pipeline("auth", "TcpServerService", self.auth_token)
-        result = self.connection_handler.exec(payload)#fails
+        payload = payloads.SLOBSPayloads().create_payload("auth", "TcpServerService", self.auth_token)
+        result = self.connection_handler.exec(payload)
         decoded = self.connection_handler._decode_sockjs_array(result)
         if 'error' in decoded:
             print(decoded)
@@ -29,5 +28,6 @@ class SLOBSClient():
             print(decoded)
             return False
 
-    def send(self):
-        return True
+    def send(self, method: str, resource: str, callback = None, *args):
+        payload = payloads.SLOBSPayloads().create_payload(method, resource, args)
+        self.connection_handler.send(payload, callback)
