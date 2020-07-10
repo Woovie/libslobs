8f4fc24d8c0417a04e8071f3a4bf6530490c32a5
Jordan Banasik <woovie@woovie.net>
2020-07-09 20:34:31 -0700

    Added type hints for create_payload
    Type hints for callable are complex, found them in typing with some good
    docs, added here.

--- common_payloads.py
+++ common_payloads.py
@@ -1,11 +1,11 @@
 import uuid
-from typing import Callable
+from typing import Callable, Any
 
 class SLOBSPayloads():
     def __init__(self):
         self.token = str(uuid.uuid1())
 
-    def create_payload(self, method: str, resource: str, *args):
+    def create_payload(self, method: str, resource: str, *args: Any):
         return {
             "jsonrpc": "2.0",
             "id": self.token,
