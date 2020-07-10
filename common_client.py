d5ee3baa44ff3160e08aecef97181081025e689f
Jordan Banasik <woovie@woovie.net>
2020-07-09 20:36:11 -0700

    Rewrote imports, added threads, added send method
    Imports with 'as' make writing code harder sometimes, I was struggling
    with 'queue', so back to the long messy method for now.
    
    Added verbosity to SLOBSClient, for future debug functionality.
    
    Added threads dict for storing threads which I create.
    
    Modified connect to return True, False. No need to make clients check.
    It should return 'o' or give a fat error.
    
    Modfiied auth to return both a bool and the payload, so you can figure
    out what's wrong.
    
    Added send, for sending a payload and setting up a callback, if wanted.
    Going to move auth to using this.
    
    Added recv, but not sure what for yet.

--- common_client.py
+++ common_client.py
@@ -1,33 +1,45 @@
-import uuid
-import libslobs.common_queue as queue
-import libslobs.common_websocket as websocket
-import libslobs.common_payloads as payloads
+import uuid, threading
+import libslobs.common_websocket
+import libslobs.common_payloads
+import libslobs.common_queue
+from typing import Callable, Any
 
 class SLOBSClient():
-    def __init__(self, api_token: str, url: str):
+    def __init__(self, api_token: str, url: str, verbosity: int = None):
         self.url = url
         self.api_token = api_token
         self.connection_string = str(uuid.uuid1())
         self.slobs_websocket_url = f"{self.url}{self.connection_string}/websocket"
-        self.connection_handler = websocket.SLOBSWebSocket(self.slobs_websocket_url)
-        self.queue = queue.SLOBSQueue()
+        self.connection_handler = libslobs.common_websocket.SLOBSWebSocket(self.slobs_websocket_url)
+        self.queue = libslobs.common_queue.SLOBSQueue()
+        self.verbosity = verbosity
+        self.threads = {}
 
     def connect(self):
-        return self.connection_handler.connect()
+        returned = self.connection_handler.connect()
+        if returned == 'o':
+            return True
+        else:
+            return False
 
     def auth(self):
-        payload = payloads.SLOBSPayloads().create_payload("auth", "TcpServerService", self.api_token)
+        payload = libslobs.common_payloads.SLOBSPayloads().create_payload("auth", "TcpServerService", self.api_token)
         result = self.connection_handler.exec(payload)
         decoded = self.connection_handler._decode_sockjs_array(result)
         if 'error' in decoded:
-            print(decoded)
-            return False
+            return False, decoded
         elif 'result' in decoded:
-            return decoded['result']
-        else:
-            print(decoded)
-            return False
+            return True, decoded
 
-    def send(self, method: str, resource: str, callback = None, *args):
-        payload = payloads.SLOBSPayloads().create_payload(method, resource, args)
-        self.connection_handler.send(payload, callback)
+    def send(self, method: str, resource: str, callback: Callable[[dict], Any] = None, *args):
+        if callback and hasattr(callback, '__call__'):# throw it in queue for callback
+            payload = libslobs.common_payloads.SLOBSPayloads().create_payload(method, resource, args)
+            payload_result = self.connection_handler.exec(payload)
+            queue_result = self.queue.add_callback(callback, payload['id'])
+
+        else:
+            return False# Some error needs to happen here.
+        
+
+    def recv(self, payload: dict):# So we will get back a dict from SLOBS here
+        return True
