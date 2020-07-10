cb054da3aaafe1ca20a0da58cbc4ac36c41079f6
Jordan Banasik <woovie@woovie.net>
2020-07-09 20:32:37 -0700

    Added returns to add_callback, added new methods
    add_callback type hinting and returns added
    
    _process_queue added to be a thread which constantly tries to process
    the entire queue (does nothing currently, just prints)
    
    _incoming added for incoming messages to be added to the queue from
    websocket.

--- common_queue.py
+++ common_queue.py
@@ -1,12 +1,23 @@
-from typing import Callable
+from typing import Callable, Any
 
 class SLOBSQueue():
     def __init__(self):
         self.incoming = []
         self.events = {}
 
-    def add_callback(self, callback: Callable[[str], str], id: str):
+    def add_callback(self, callback: Callable[[dict], Any], id: str):
         self.events[id] = callback
+        if id in self.events:
+            return True
+        else:
+            return False# This is going to be a major problem, and likely python will have fucked up much greater if this happens.
 
     def remove_callback(self, id: str):
         self.events.pop(id)
+    
+    def _process_queue(self):
+        for message in self.incoming:
+            print(message)
+        
+    def _incoming(self, message: dict):
+        self.incoming.append(message)
