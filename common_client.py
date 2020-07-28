67bbcf6e83d11665a3a91c9b3e74b7f127acbb00
Jordan Banasik <woovie@woovie.net>
2020-07-27 19:54:36 -0700

    Moved connect() into Client() class
    - Added configparser
    - Renamed SLOBSClient to Client
    - Renamed connection_handler to websocket
    - Added config variable
    - Added authid being set on init
    - Added connect() method
    - Added start_threads() method
    - Added auth() method which contains a testing print for now
    
    First commit of a refactor

--- common_client.py
+++ common_client.py
@@ -1,19 +1,37 @@
-import uuid, threading, asyncio
+import uuid, threading, asyncio, configparser
 import libslobs.common_websocket
 import libslobs.common_payloads
 import libslobs.common_queue
+import libslobs
 from typing import Callable, Any
 
-class SLOBSClient():
+class Client():
     def __init__(self, api_token: str, url: str, verbosity: int = None):
         self.url = url
         self.api_token = api_token
         self.connection_string = str(uuid.uuid1())
         self.slobs_websocket_url = f"{self.url}{self.connection_string}/websocket"
-        self.connection_handler = libslobs.common_websocket.SLOBSWebSocket(self.slobs_websocket_url)
-        self.queue = libslobs.common_queue.SLOBSQueue()
+        self.websocket = libslobs.common_websocket.WebSocket(self.slobs_websocket_url)
+        self.queue = libslobs.common_queue.Queue()
         self.verbosity = verbosity
         self.threads = {}
         self.threads['queue_processor'] = threading.Thread(target=self.queue.start)
-        self.threads['websocket_recv'] = threading.Thread(target=self.connection_handler.start, args=[self.queue])
-        self.authid = None
+        self.threads['websocket_recv'] = threading.Thread(target=self.websocket.start, args=[self.queue])
+        self.config = configparser.ConfigParser()
+        self.config.read('settings.ini')
+        self.authid = self.config['slobs-client']['api-token']
+    
+    def connect(self):
+        output = self.websocket.connect()
+        if output == 'o':
+            self.start_threads()
+            self.auth()
+
+    def start_threads(self):
+        self.threads['queue_processor'].start()
+        self.threads['websocket_recv'].start()
+
+    def auth(self):
+        payload = libslobs.common_payloads.create_payload('auth', 'TcpServerService', self.authid)
+        auth_output = self.websocket.exec_return(payload)
+        print(auth_output)
