7e11e85f9b335e0507cbd3254778c35ca472af60
Jordan Banasik <woovie@woovie.net>
2020-07-27 21:33:09 -0700

    Major refactor
    Modified all imports to be shorter, added random
    Added server number generator
    Modified websocket_url to include server number
    modified connect to return if auth succeeded
    Modified auth to return whether or not it worked and print the error as
    to why it did not

--- common_client.py
+++ common_client.py
@@ -1,7 +1,8 @@
-import uuid, threading, asyncio, configparser
-import libslobs.common_websocket
-import libslobs.common_payloads
-import libslobs.common_queue
+import uuid, threading, asyncio, configparser, random
+import libslobs.common_websocket as websocket
+import libslobs.common_payloads as payloads
+import libslobs.common_queue as queue
+import libslobs.common_sockjs as sockjs
 from typing import Callable, Any
 
 class Client():
@@ -9,9 +10,10 @@ class Client():
         self.url = url
         self.api_token = api_token
         self.connection_string = str(uuid.uuid1())
-        self.slobs_websocket_url = f"{self.url}{self.connection_string}/websocket"
-        self.websocket = libslobs.common_websocket.WebSocket(self.slobs_websocket_url)
-        self.queue = libslobs.common_queue.Queue()
+        self.server_number = f"{random.randrange(1, 10**3):03}"
+        self.websocket_url = f"{self.url}/{self.server_number}/{self.connection_string}/websocket"
+        self.websocket = websocket.WebSocket(self.websocket_url)
+        self.queue = queue.Queue()
         self.verbosity = verbosity
         self.threads = {}
         self.threads['queue_processor'] = threading.Thread(target=self.queue.start)
@@ -20,17 +22,26 @@ class Client():
     
     def connect(self):
         output = self.websocket.connect()
-        print(output)
         if output == 'o':
             self.start_threads()
-            self.auth()
+            auth_result = self.auth()
+            if auth_result:
+                return True
 
     def start_threads(self):
         self.threads['queue_processor'].start()
         self.threads['websocket_recv'].start()
 
     def auth(self):
-        payload = libslobs.common_payloads.create_payload('auth', 'TcpServerService', self.api_token)
+        payload = payloads.create_payload('auth', 'TcpServerService', self.api_token)
         self.authid = payload['id']
         auth_output = self.websocket.exec_return(payload)
-        print(auth_output)
+        if 'result' in auth_output:
+            if auth_output['result']:
+                return True
+            else:
+                print(auth_output)
+                return False
+        else:
+            print(auth_output)
+            return False
