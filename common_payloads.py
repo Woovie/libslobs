49b40fe0ed3cb8d2fb7bb5581cdd9d1fc4c9c70d
Jordan Banasik <woovie@woovie.net>
2020-07-27 20:01:40 -0700

    Turn create_payload into just a function
    The class was overkill for creating a payload. Using just a function
    now.

--- common_payloads.py
+++ common_payloads.py
@@ -1,17 +1,14 @@
 import uuid
 from typing import Callable, Any
 
-class SLOBSPayloads():
-    def __init__(self):
-        self.token = str(uuid.uuid1())
-
-    def create_payload(self, method: str, resource: str, *args: Any):
-        return {
-            "jsonrpc": "2.0",
-            "id": self.token,
-            "method": method,
-            "params": {
-                "resource": resource,
-                "args": list(args) if args else None
-            }
+def create_payload(method: str, resource: str, *args: Any):
+    token = str(uuid.uuid1())
+    return {
+        "jsonrpc": "2.0",
+        "id": token,
+        "method": method,
+        "params": {
+            "resource": resource,
+            "args": list(args) if args else None
         }
+    }
